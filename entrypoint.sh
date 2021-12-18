993aafd203bb63c913ce06a37b79a2de062b42b9
fantasylidong <fantasylidong@gmail.com>
2021-12-18 23:54:24 +0800

    Update entrypoint.sh
    Another way to use third party maps.
    Very useful when you have sourcemod plugins needed.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,11 @@
 # Update Game
 ./steamcmd.sh +login anonymous +force_install_dir ./l4d2 +app_update 222860 +quit
 
+#Softlink l4d2 maps to addons folder
+#It would more convenience while you want add custom map. Exspecially when you have sourcemod plugins
+#you just need mount your extra map folder to docker container /map . 
+ln  -s  /map/*  l4d2/left4dead2/addons/
+
 # Server Config
 cat > l4d2/left4dead2/cfg/server.cfg <<EOF
 hostname "${HOSTNAME}"
