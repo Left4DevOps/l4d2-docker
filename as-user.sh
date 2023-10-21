1fd325f0061b9e69899a09f12728849cc8ba5a50
Harry <hazmeister@gmail.com>
2023-10-21 20:43:08 +0100

    64bit not used

--- as-user.sh
+++ as-user.sh
@@ -4,9 +4,8 @@ curl https://media.steampowered.com/installer/steamcmd_linux.tar.gz | tar -xzvf 
 ./steamcmd.sh +quit
 
 # Symlink steamclient.so
-mkdir -p .steam/sdk32/ .steam/sdk64/
+mkdir -p .steam/sdk32/
 ln -s ~/linux32/steamclient.so ~/.steam/sdk32/steamclient.so
-ln -s ~/linux64/steamclient.so ~/.steam/sdk64/steamclient.so
 
 # Install game
 ./steamcmd.sh +force_install_dir "./${INSTALL_DIR}" +login anonymous +app_update "${GAME_ID}" +quit
