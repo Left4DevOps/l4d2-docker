eadd8df1ae209691afccfafbc8870df675e1adcb
Harry <hazmeister@gmail.com>
2024-04-13 00:26:19 +0100

    autoupdate not working on l4d

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
+# Auto update flag not working on L4D
+if [ "${INSTALL_DIR}" = "l4d" ]; then
+    ./steamcmd.sh +runscript update.txt
+fi
+
 cd "${INSTALL_DIR}" || exit 50
 
 if [ $# -gt 0 ]; then
     ./srcds_run "$@"
 else
     STARTUP=("./srcds_run")
-    STARTUP+=("-autoupdate -steam_dir $HOME -steamcmd_script $HOME/update.txt")
     STARTUP+=("+map \"$DEFAULT_MAP $DEFAULT_MODE\"")
     STARTUP+=("+sv_logecho 1")
     STARTUP+=("+hostname \"${HOSTNAME}\"")
     STARTUP+=("+sv_region ${REGION}")
     STARTUP+=("+motd_enabled 0")
+
+    if [ "${INSTALL_DIR}" = "l4d2" ]; then
+            STARTUP+=("-autoupdate -steam_dir $HOME -steamcmd_script $HOME/update.txt")
+        fi
 
     if [ -n "${RCON_PASSWORD}" ]; then
         STARTUP+=("+rcon_password \"${RCON_PASSWORD}\"")
