965452f815a728a12abd3d3d13b16cbec5fbb657
Harry <hazmeister@gmail.com>
2024-04-18 00:39:05 +0100

    Reformat and add telnet client

--- as-root.sh
+++ as-root.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-microdnf -y install SDL2.i686 libcurl.i686 glibc-langpack-en tar
+microdnf -y install SDL2.i686 \
+    libcurl.i686 \
+    glibc-langpack-en \
+    tar \
+    telnet
 microdnf -y update
 microdnf clean all
 
