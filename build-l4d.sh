f83a166df9f2c13183fd5d7c98ec1797323d7cf1
Harry <hazmeister@gmail.com>
2024-09-28 13:57:43 +0100

    Plain build output

--- build-l4d.sh
+++ build-l4d.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -t left4devops/l4d \
+docker build --progress plain -t left4devops/l4d \
   --build-arg GAME_ID=222840 \
   --build-arg INSTALL_DIR="l4d" \
   --build-arg DEFAULT_MAP="l4d_river01_docks" \
