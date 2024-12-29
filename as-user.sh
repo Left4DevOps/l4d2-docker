ac2105ead52081b478b96ea9961e8474587bb522
Harry <hazmeister@gmail.com>
2024-12-29 22:16:55 +0000

    Switch platform for first install

--- as-user.sh
+++ as-user.sh
@@ -22,4 +22,16 @@ echo """force_install_dir "/home/louis/${INSTALL_DIR}"
 login anonymous
 app_update ${GAME_ID}
 quit""" > update.txt
-./steamcmd.sh +runscript update.txt
+if [ "${INSTALL_DIR}" = "l4d2" ]; then
+  # https://github.com/ValveSoftware/steam-for-linux/issues/11522
+  echo """force_install_dir "/home/louis/${INSTALL_DIR}"
+  login anonymous
+  @sSteamCmdForcePlatformType windows
+  app_update ${GAME_ID}
+  @sSteamCmdForcePlatformType linux
+  app_update ${GAME_ID} validate
+  quit""" > first-install-l4d2.txt
+  ./steamcmd.sh +runscript first-install-l4d2.txt
+else
+  ./steamcmd.sh +runscript update.txt
+fi
