a082ac68a9312576c629693623bcaa48e68e069a
Harry <hazmeister@gmail.com>
2024-12-29 22:15:58 +0000

    Install steamcmd first

--- as-user.sh
+++ as-user.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Install steamcmd
+mkdir -p .steam/sdk32/
+ln -s ~/linux32/steamclient.so ~/.steam/sdk32/steamclient.so
+curl https://media.steampowered.com/installer/steamcmd_linux.tar.gz | tar -xzvf -
+
 # Convenient symlinks for mount points
 if [ "${INSTALL_DIR}" = "l4d2" ]; then
     GAME_DIR="${INSTALL_DIR}/left4dead2"
@@ -11,11 +16,6 @@ fi
 mkdir -p ./"${GAME_DIR}"
 ln -s /addons "./${GAME_DIR}/addons"
 ln -s /cfg    "./${GAME_DIR}/cfg"
-
-# Install steamcmd
-mkdir -p .steam/sdk32/
-ln -s ~/linux32/steamclient.so ~/.steam/sdk32/steamclient.so
-curl https://media.steampowered.com/installer/steamcmd_linux.tar.gz | tar -xzvf -
 
 # Install game
 echo """force_install_dir "/home/louis/${INSTALL_DIR}"
